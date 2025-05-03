57f7193873ed7bc3f7366934e69fa631b4159c0b
MadeAditya02 <imadeaditya4@gmail.com>
2025-05-03 16:01:28 +0800

    Temporary disable unnecesary testing

--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -10,16 +10,16 @@ class ProfileUpdateTest extends TestCase
 {
     use RefreshDatabase;
 
-    // public function test_profile_page_is_displayed()
-    // {
-    //     $user = User::factory()->create();
+    public function test_profile_page_is_displayed()
+    {
+        $user = User::factory()->create();
 
-    //     $response = $this
-    //         ->actingAs($user)
-    //         ->get('/settings/profile');
+        $response = $this
+            ->actingAs($user)
+            ->get('/settings/profile');
 
-    //     $response->assertOk();
-    // }
+        $response->assertOk();
+    }
 
     // public function test_profile_information_can_be_updated()
     // {
